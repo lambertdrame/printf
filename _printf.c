c11ea5b00ebac8743fb226ca548ab1388d63b689
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-24 10:03:34 -0700

    Test

--- _printf.c
+++ _printf.c
@@ -9,7 +9,8 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, count = 0, consecutive_percentage = 0;
+	int i = 0;
+	int count = 0;
 	va_list args;
 
 	if (!format || (format[0] == '%' && format[1] == '\0'))
@@ -20,44 +21,22 @@ int _printf(const char *format, ...)
 		if (format[i] == '%')
 		{
 			i++;
-			consecutive_percentage = 0;
 			while (format[i] == ' ')
 				i++;
-			while (format[i] == '%')
+			if (format[i] == 's')
+				count += print_str(args);
+			else if (format[i] == 'c')
 			{
-				i++;
-				consecutive_percentage++;
-			}
-			if (consecutive_percentage % 2 == 0)
-			{
-				for (j = 0; j < consecutive_percentage / 2; j++)
-				{
-					write(1, "%%", 2);
-					count += 2;
-				}
-				if (format[i] == 's')
-					count += print_str(args);
-				else if (format[i] == 'c')
-				{
-					print_char(args);
-					count++;
-				}
-				else if (format[i] == '\0')
-				{
-					va_end(args);
-					return (-1);
-				}
-				else
-				{
-					va_end(args);
-					return (-1);
-				}
-			}
-			else
-			{
-				write(1, "%", 1);
+				print_char(args);
 				count++;
 			}
+			else if (format[i] == '%')
+			{
+				write(1, format + i, 1);
+				count++;
+			}
+			else
+				return (-1);
 		}
 		else
 		{
