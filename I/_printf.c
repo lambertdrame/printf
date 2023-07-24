c5847e9b9dc52b3f969b78f6b9b858987a61c050
Iyad M. Mahrous <iyad.m.mahrous@gmail.com>
2023-07-24 12:29:37 -0700

    After Buffer

---
+++ I/_printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _printf - Printing something to screen
+ * @format: String to print
+ * @... : arguments
+ *
+ * Return: number of characters printed, -1 if error
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list args;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			while (format[i] == ' ')
+				i++;
+			if (format[i] == 's')
+				count += print_str(args);
+			else if (format[i] == 'c')
+			{
+				print_char(args);
+				count++;
+			}
+			else if (format[i] == '%')
+			{
+				write(1, format + i, 1);
+				count++;
+			}
+			else
+			{
+				va_end(args);
+				return (-1);
+			}
+		}
+		else
+		{
+			write(1, format + i, 1);
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
